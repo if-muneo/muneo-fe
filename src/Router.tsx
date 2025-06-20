afd71090fee6bd46799ccb321a8bee0ebf3cc3a6
minkyeong <stl9955@naver.com>
2025-06-20 09:38:57 +0900

    fix: header & router 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -9,11 +9,12 @@ import AddonListPage from './pages/AddonListPage.tsx';
 import AdminAddOnCreatePage from './pages/AdminAddOnCreatePage.tsx';
 import AdminAddOnGroupPage from './pages/AdminAddOnGroupPage.tsx';
 import AdminAddOnGroupCreatePage from './pages/AdminAddOnGroupCreatePage.tsx';
+import AdminMplanCreatePage from './pages/AdminMplanCreatePage.tsx';
 import PageTransition from './components/PageTransition.tsx';
 import MplanListPage from "./pages/MplanListPage.tsx";
 import UserMplanDetailPage from "./pages/UserMplanDetailPage.tsx";
 import MyPage from "./pages/MyPage.tsx";
-import PlanListPage from "./pages/PlanListPage.tsx";
+import AdminPlanListPage from "./pages/AdminPlanListPage.tsx";
 
 const PageLayout: React.FC = () => {
   return (
@@ -29,19 +30,20 @@ const Router = () => {
       <Route element={<PageLayout />}>
         <Route path="/" element={<StartPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
           <Route path="/plans" element={<MplanListPage />} />
           <Route path="/addons" element={<AddonListPage />} />
           <Route path="/mplan/:id" element={<UserMplanDetailPage />} />
           <Route path="/mypage" element={<MyPage />} />
-          <Route path="/admin" element={<AdminPage />} />
-          <Route path="/admin/services" element={<AdminAddOnListPage />} />
-          <Route path="/admin/services/create" element={<AdminAddOnCreatePage />} />
-          <Route path="/admin/groups" element={<AdminAddOnGroupPage />} />
-          <Route path="/admin/groups/create" element={<AdminAddOnGroupCreatePage />} />
-          <Route path="/admin/plans" element={<PlanListPage />} />
       </Route>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/admin/services" element={<AdminAddOnListPage />} />
+        <Route path="/admin/services/create" element={<AdminAddOnCreatePage />} />
+        <Route path="/admin/groups" element={<AdminAddOnGroupPage />} />
+        <Route path="/admin/groups/create" element={<AdminAddOnGroupCreatePage />} />
+        <Route path="/admin/plans/create" element={<AdminMplanCreatePage />} />
+        <Route path="/admin/plans" element={<AdminPlanListPage />} />
     </Routes>
   );
 };
